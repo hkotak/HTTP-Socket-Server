daba48e1a016182fe0f5594905fdf3709eabc535
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-08-11 10:01:06 -1000

    console logging things

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const uriContent = require('./uri-content');
 const port = 8080;
 
 const server = net.createServer(function(request) {
-  console.log("request received", request);
+  console.log("request received");
+
+  request.on('data', function(data) {
+
+    let stringData = data.toString();
+    //console.log("stringData: ", stringData);
+    let arrayData = stringData.split('\n');
+    //console.log("arrayData: ", arrayData);
+    let firstLineElements = arrayData[0].split(" ");
+    //console.log("firstLineElements: ", firstLineElements);
+    let reqMethod = firstLineElements[0];
+    //console.log("reqMethod: ", reqMethod);
+    let reqFile = firstLineElements[1];
+    console.log("reqFile: ", reqFile);
 
 });
 
